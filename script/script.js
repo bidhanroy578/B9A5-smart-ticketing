d40ebbbc4df39fe39bb09b25e08f90a7b8c95b5c
Bidhan Roy <bidhanroy578@gmail.com>
2024-10-06 03:19:03 +0600

    script.js final touch done

--- script/script.js
+++ script/script.js
@@ -1,22 +1,22 @@
 function onClick(element) {
-    let sit = document.getElementById('seat-Number').innerText
-    let sitValue = parseFloat(sit)
-    let list = element.classList.value.split(' ')
-    const sitLeft = document.getElementById('sit-left')
+    let sit = document.getElementById('seat-Number').innerText;
+    let sitValue = parseFloat(sit);
+    let list = element.classList.value.split(' ');
+    const sitLeft = document.getElementById('sit-left');
     if (list.includes('btnSelect')) {
-        console.log('you already selected the btn ')
+        console.log('you already selected the btn ');
     } else {
         if (sitValue < 4) {
-            element.classList.add('btnSelect')
-            sitValue = sitValue + 1
-            document.getElementById('seat-Number').innerText = sitValue
-            sitLeft.innerText = 40 - sitValue
-            addTicketToTable(element.id)
-            updateTotalPrice()
-            enableCouponApplyButton()
-            document.getElementById('next-page-button').classList.remove('btn-disabled')
+            element.classList.add('btnSelect');
+            sitValue = sitValue + 1;
+            document.getElementById('seat-Number').innerText = sitValue;
+            sitLeft.innerText = 40 - sitValue;
+            addTicketToTable(element.id);
+            updateTotalPrice();
+            enableCouponApplyButton();
+            document.getElementById('next-page-button').classList.remove('btn-disabled');
         } else {
-            alert("Limit excceeded, Can't select more then four tickets at once . ")
+            alert("Limit excceeded, Can't select more then four tickets at once . ");
         }
     }
 }
