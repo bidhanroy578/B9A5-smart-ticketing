6031d7bfa67ef705d70a5e8aa27ca28e17c21419
Bidhan Roy <bidhanroy578@gmail.com>
2024-10-06 03:21:58 +0600

    utility.js final touch done

--- script/utility.js
+++ script/utility.js
@@ -1,51 +1,40 @@
 function addTicketToTable(sitName) {
-    let item = document.getElementById('no-ticket-warning')
-    let tbody = document.getElementById('ticket-tbody')
-    item.classList.add('hidden')
-    let tr = document.createElement('tr')
-    let th = document.createElement('th')
-    th.innerText = sitName
-    let td1 = document.createElement('td')
-    td1.innerText = "Economy"
-    let td2 = document.createElement('td')
-    td2.innerText = "550"
-    //     tr.innerHTML = ` <th>sitname</th>
-    // <td>Economy</td>
-    // <td>550</td> `
-    tr.appendChild(th)
-    tr.appendChild(td1)
-    tr.appendChild(td2)
-    tbody.appendChild(tr)
+    let item = document.getElementById('no-ticket-warning');
+    let tbody = document.getElementById('ticket-tbody');
+    item.classList.add('hidden');
+    let tr = document.createElement('tr');
+    let th = document.createElement('th');
+    th.innerText = sitName;
+    let td1 = document.createElement('td');
+    td1.innerText = "Economy";
+    let td2 = document.createElement('td');
+    td2.innerText = "550";
+    tr.appendChild(th);
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tbody.appendChild(tr);
 }
-
 function updateTotalPrice() {
-
-    let ticket = parseFloat(document.getElementById('seat-Number').innerText)
-    let price = 550 * ticket
-
-    document.getElementById('total-price').innerText = price
-    document.getElementById('grand-total').innerText = price
-
+    let ticket = parseFloat(document.getElementById('seat-Number').innerText);
+    let price = 550 * ticket;
+    document.getElementById('total-price').innerText = price;
+    document.getElementById('grand-total').innerText = price;
 }
 function enableCouponApplyButton() {
-    let ticket = parseFloat(document.getElementById('seat-Number').innerText)
+    let ticket = parseFloat(document.getElementById('seat-Number').innerText);
     if (ticket > 1) {
-        document.getElementById('coupon-apply-button').classList.remove('btn-disabled')
+        document.getElementById('coupon-apply-button').classList.remove('btn-disabled');
     }
-    // else{
-    //     document.getElementById('coupon-apply-button').classList.add('btn-disabled')
-    // }
 }
 function disableCouponApplyButton() {
-    let ticket = parseFloat(document.getElementById('seat-Number').innerText)
+    let ticket = parseFloat(document.getElementById('seat-Number').innerText);
     if (ticket > 1) {
-        document.getElementById('coupon-apply-button').classList.add('btn-disabled')
+        document.getElementById('coupon-apply-button').classList.add('btn-disabled');
     }
 }
 function grandTotalPriceAfterAppliedCoupon() {
-    let total = parseFloat(document.getElementById('total-price').innerText)
-    let discounted = parseFloat(document.getElementById('discount-price').innerText)
-    let finalPrice = total - discounted
-    document.getElementById('grand-total').innerText = finalPrice
-
+    let total = parseFloat(document.getElementById('total-price').innerText);
+    let discounted = parseFloat(document.getElementById('discount-price').innerText);
+    let finalPrice = total - discounted;
+    document.getElementById('grand-total').innerText = finalPrice;
 }
